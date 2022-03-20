51b0d2d1ab21da11c12030b85815f1859cfbe2b6
awesomeman007 <suparuek.ping@gmail.com>
2022-03-20 21:30:49 +0700

    otp swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,22 @@
 import { Body, Controller, Post, Get, Param } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import {ApiParam, ApiBody, ApiOkResponse, ApiUnauthorizedResponse} from '@nestjs/swagger'
+import { ApiParam, ApiBody, ApiOkResponse, ApiUnauthorizedResponse, ApiBadRequestResponse } from '@nestjs/swagger'
+import { OtpDTO } from './dto/auth.dto'
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiParam({ name: 'phoneNumber', type: String, description: "user's phone number" })
+  @ApiOkResponse({ description: 'OTP Successfully Sent' })
+  @ApiBadRequestResponse({ description: 'Failed Requesting OTP' })
   @Get('/getOtp/:phoneNumber')
   getOTP(@Param('phoneNumber') phoneNumber: string) {
     return this.authService.getOtp(phoneNumber)
   }
 
-//   @ApiBody({ type: {} })
-  @ApiOkResponse({ description: "Verified Successfully" })
-  @ApiUnauthorizedResponse({ description: "Failed Verifying OTP" })
+  @ApiBody({ type: OtpDTO })
+  @ApiOkResponse({ description: 'Verified Successfully' })
+  @ApiUnauthorizedResponse({ description: 'Failed Verifying OTP' })
   @Post('/verifyOtp')
   verifyOTP(@Body() body: { token: string; pin: string }) {
     return this.authService.verifyOtp(body.token, body.pin)
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,13 @@
-// import { ApiProperty } from '@nestjs/swagger'
-// import {IsString}
+import { ApiProperty } from '@nestjs/swagger'
+import {IsString} from 'class-validator'
 // import { Type } from 'class-transformer'
 
-// class OtpDTO {
-//     @ApiProperty()
-//     @IsString()
+export class OtpDTO {
+    @ApiProperty()
+    @IsString()
+    token: String
 
-
-// }
+    @ApiProperty({minLength: 4, maxLength: 4})
+    @IsString()
+    pin: String
+}
