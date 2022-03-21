8c814c43fe0ef9f02e0cb1802a10a108e90d7f7e
siradheek <siradhee_k@hotmail.com>
2022-03-21 19:03:03 +0700

    add update personal info

--- src/user/dto/update-user.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types'
-
-export class UpdateRelationshipDto {
-  eid: number
-  cid: number
-}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,49 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  UsePipes,
+  ValidationPipe,
+  ParseBoolPipe,
+  Put,
+} from '@nestjs/common'
 import { UserService } from './user.service'
-import { UpdateRelationshipDto } from './dto/update-user.dto'
+import { UpdatePersonalInfoDto, UpdateRelationshipDto } from './dto/user.dto'
+import { CreateUserDto } from './dto/user.dto'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('caretaker/:eid')
-  async getCaretakers(@Param('eid') uid: number) {
-    return await this.userService.findCaretakerByElderlyId(uid)
+  async getCaretakersByElderlyId(@Param('eid') eid: number) {
+    return await this.userService.findCaretakerByElderlyId(eid)
   }
   @Get('elderly/:cid')
-  async getElderly(@Param('cid') cid: number) {
+  async getElderlyByCaretakerId(@Param('cid') cid: number) {
     return await this.userService.findElderlyByCaretakerId(cid)
   }
   @Post('relationship')
-  async addRelationship(@Body() body: UpdateRelationshipDto) {
-    return await this.userService.addRelationship(body)
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async createRelationship(@Body() body: UpdateRelationshipDto) {
+    return await this.userService.createRelationship(body)
   }
   @Delete('relationship')
-  async removeRelationship(@Body() body: UpdateRelationshipDto) {
-    return await this.userService.removeRelationship(body)
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async deleteRelationship(@Body() body: UpdateRelationshipDto) {
+    return await this.userService.deleteRelationship(body)
+  }
+  @Post()
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async createUser(@Body() body: CreateUserDto) {
+    return await this.userService.createUser(body)
+  }
+  @Put()
+  @UsePipes(new ValidationPipe({ transform: true, transformOptions: { excludeExtraneousValues: true } }))
+  async updatePersonalInfo(@Body() body: UpdatePersonalInfoDto) {
+    return await this.userService.updateUser(body)
   }
 }
