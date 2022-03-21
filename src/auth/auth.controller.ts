0fd1bf12b5ecbe0a2b66626c6cd2008ab5e39bb4
awesomeman007 <suparuek.ping@gmail.com>
2022-03-21 17:38:23 +0700

    remove unused code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,14 +22,3 @@ export class AuthController {
     return this.authService.verifyOtp(body.token, body.pin)
   }
 }
-
-// @Post("/login")
-//   async login(@Body() loginUserDto: LoginUserDto, @Res() res): Promise<string> {
-//     const user = await this.userService.login(loginUserDto.username, loginUserDto.password)
-//     return res.status(201).json({
-//       statusCode: 201,
-//       message: "Login successfully",
-//       jwt: this.authService.generateJWT(user["_id"], "User"),
-//       is_thai_language: user["is_thai_language"],
-//     })
-//   }
