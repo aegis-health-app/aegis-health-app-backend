9cadc3a81d90f57d7f6cbb6179d0fa4ecb93a9b7
Natthorn <tum210544@gmail.com>
2022-03-23 00:56:02 +0700

    change name from elderlyHome to home

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
-import { OtpModule } from './otp/otp.module';
 import { UserModule } from './user/user.module';
+import { HomeModule } from './home/home.module';
+import { OtpModule } from './otp/otp.module'
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -25,6 +27,7 @@ import { UserModule } from './user/user.module';
         synchronize: false,
       }),
     }),
+    HomeModule,
     OtpModule,
     UserModule,
   ],
