60c554600f9a6cc6fa317c02b0c863591c5e7b14
patarapornkan <patt.anantarangsi@gmail.com>
2022-03-22 17:24:46 +0700

    fix: update filepath

--- src/link/link.service.ts
+++ src/link/link.service.ts
@@ -2,7 +2,7 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
-import { ElderlyCode, UserInfo } from './link.interface'
+import { ElderlyCode, UserInfo } from './interfaces/link.interface'
 import Hashids from 'hashids';
 
 @Injectable()
