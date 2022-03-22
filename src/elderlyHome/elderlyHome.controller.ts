0d67b4b213850f4a17a88183af0d50c1f1a29c89
Natthorn <tum210544@gmail.com>
2022-03-23 00:52:07 +0700

    add get caretaker home api

--- src/elderlyHome/elderlyHome.controller.ts
+++ src/elderlyHome/elderlyHome.controller.ts
@@ -29,3 +29,8 @@ export class ElderlyHomeController {
         const selectedModule = await this.homeService.addModule(req.user.uid, moduleid)
         return selectedModule
     }
+    @Get('/caretakerHome')
+    async getCaretakerHome(@Req() req): Promise<CaretakerHomeDTO> {
+        const elderly = await this.homeService.getCaretakerHome(req.user.uid)
+        return elderly
+    }
