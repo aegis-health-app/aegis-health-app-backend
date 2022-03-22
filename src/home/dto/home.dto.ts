2c6be3c05dc4d9cfaced3409b53654d28a04fe83
Natthorn <tum210544@gmail.com>
2022-03-23 00:56:08 +0700

    add dto

---
+++ src/home/dto/home.dto.ts
@@ -0,0 +1,97 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsDate, IsNumber, IsString } from 'class-validator'
+
+export class ElderlyHomeDTO {
+    @ApiProperty()
+    @IsString()
+    dname: string
+
+    @ApiProperty()
+    @IsString()
+    imageid: string
+
+    @IsNumber({},{each: true})
+    listModuleid: number[]
+}
+
+export class ElderlyDTO {
+    @ApiProperty()
+    @IsNumber()
+    uid: number
+
+    @ApiProperty()
+    @IsString()
+    dname: string
+
+    @ApiProperty()
+    @IsString()
+    imageid: string
+}
+
+export class CaretakerHomeDTO {
+    @ApiProperty()
+    @IsString()
+
+    dname: string
+
+    @ApiProperty()
+    @IsString()
+    imageid: string
+
+    @ApiProperty({ type: [ElderlyDTO] })
+    listElderly: Array<ElderlyDTO>
+}
+
+export class ElderlyInfoDTO {
+    @ApiProperty()
+    @IsString()
+    imageid: string
+
+    @ApiProperty()
+    @IsString()
+    fname: string
+
+    @ApiProperty()
+    @IsString()
+    lname: string
+
+    @ApiProperty()
+    @IsString()
+    dname: string
+
+    @ApiProperty()
+    @IsString()
+    gender: string
+
+    @ApiProperty()
+    @IsDate()
+    bday: Date
+
+    @ApiProperty()
+    @IsString()
+    healthCondition: string
+
+    @ApiProperty()
+    @IsString()
+    bloodType: string
+
+    @ApiProperty()
+    @IsString()
+    personalMedication: string
+
+    @ApiProperty()
+    @IsString()
+    allergy: string
+
+    @ApiProperty()
+    @IsString()
+    vaccine: string
+
+    @ApiProperty()
+    @IsString()
+    phone: string
+
+    @ApiProperty()
+    @IsNumber({},{each: true})
+    listModuleid: number[]
+}
