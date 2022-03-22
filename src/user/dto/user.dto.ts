ca38a12b605592d352ddfdf4c305c92574a85a92
siradheek <siradhee_k@hotmail.com>
2022-03-22 12:33:58 +0700

    add swagger docs

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,33 +1,54 @@
-import { PartialType } from '@nestjs/mapped-types'
+import { ApiProperty, PartialType } from '@nestjs/swagger'
 import { Exclude, Expose, Type } from 'class-transformer'
-import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsNumberString, IsOptional, IsPhoneNumber } from 'class-validator'
+import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPhoneNumber } from 'class-validator'
 import { PersonalInfo } from '../user.interface'
 
 export class CreateUserDto extends PersonalInfo {
+  @ApiProperty()
   @IsNotEmpty()
   @IsPhoneNumber('TH')
   phone: string
+  @ApiProperty()
   @IsNotEmpty()
   password: string
 }
 export class UpdateRelationshipDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   eid: number
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   cid: number
 }
+export class GetUserDto extends PersonalInfo {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  uid: number
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsPhoneNumber('TH')
+  phone: string
+}
 
 export class UpdatePersonalInfoDto extends PartialType(PersonalInfo) {
+  @ApiProperty()
   @Expose()
   @IsNotEmpty()
-  @IsNumberString()
+  @IsNumber()
   uid: number
 }
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty()
   @IsNotEmpty()
-  @IsNumberString()
+  @IsNumber()
+  uid: number
+}
+
+export class CreateUserResponse {
+  @ApiProperty()
   uid: number
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,49 +1,62 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  UsePipes,
-  ValidationPipe,
-  ParseBoolPipe,
-  Put,
-} from '@nestjs/common'
+import { Controller, Get, Post, Body, Param, Delete, UsePipes, ValidationPipe, Put, HttpCode } from '@nestjs/common'
 import { UserService } from './user.service'
-import { UpdatePersonalInfoDto, UpdateRelationshipDto } from './dto/user.dto'
+import { CreateUserResponse, GetUserDto, UpdatePersonalInfoDto, UpdateRelationshipDto } from './dto/user.dto'
 import { CreateUserDto } from './dto/user.dto'
+import { ApiBadRequestResponse, ApiOkResponse, refs } from '@nestjs/swagger'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get('caretaker/:eid')
-  async getCaretakersByElderlyId(@Param('eid') eid: number) {
-    return await this.userService.findCaretakerByElderlyId(eid)
+  @Get(':id')
+  @ApiOkResponse({ type: GetUserDto })
+  @ApiBadRequestResponse({ description: 'User not found' })
+  async getUser(@Param('id') id: number): Promise<GetUserDto> {
+    const user = await this.userService.findOne({ uid: id }, { shouldExist: true })
+    return this.userService.schemaToDto(user)
   }
-  @Get('elderly/:cid')
+  @Get('/relationship/caretaker/:eid')
+  @ApiOkResponse({ type: [GetUserDto] })
+  @ApiBadRequestResponse({ description: 'User not found' })
+  async getCaretakersByElderlyId(@Param('eid') eid: number) {
+    const users = await this.userService.findCaretakerByElderlyId(eid)
+    return users.map((user) => this.userService.schemaToDto(user))
+  }
+  @Get('/relationship/elderly/:cid')
+  @ApiOkResponse({ type: [GetUserDto] })
+  @ApiBadRequestResponse({ description: 'User not found' })
   async getElderlyByCaretakerId(@Param('cid') cid: number) {
-    return await this.userService.findElderlyByCaretakerId(cid)
+    const users = await this.userService.findElderlyByCaretakerId(cid)
+    return users.map((user) => this.userService.schemaToDto(user))
   }
   @Post('relationship')
+  @HttpCode(201)
+  @ApiBadRequestResponse({ description: 'Relatioship already exists' })
+  @ApiOkResponse({ type: GetUserDto })
   @UsePipes(new ValidationPipe({ transform: true }))
   async createRelationship(@Body() body: UpdateRelationshipDto) {
-    return await this.userService.createRelationship(body)
+    const createdUser = await this.userService.createRelationship(body)
+    return this.userService.schemaToDto(createdUser)
   }
   @Delete('relationship')
+  @ApiOkResponse({ type: GetUserDto })
   @UsePipes(new ValidationPipe({ transform: true }))
   async deleteRelationship(@Body() body: UpdateRelationshipDto) {
-    return await this.userService.deleteRelationship(body)
+    const updatedUser = await this.userService.deleteRelationship(body)
+    return this.userService.schemaToDto(updatedUser)
   }
   @Post()
+  @HttpCode(201)
+  @ApiOkResponse({ type: CreateUserResponse })
+  @ApiBadRequestResponse({ description: 'Phone number already exists' })
   @UsePipes(new ValidationPipe({ transform: true }))
   async createUser(@Body() body: CreateUserDto) {
     return await this.userService.createUser(body)
   }
   @Put()
+  @ApiOkResponse({ type: GetUserDto })
   @UsePipes(new ValidationPipe({ transform: true, transformOptions: { excludeExtraneousValues: true } }))
   async updatePersonalInfo(@Body() body: UpdatePersonalInfoDto) {
-    return await this.userService.updateUser(body)
+    const updatedUser = await this.userService.updateUser(body)
+    return this.userService.schemaToDto(updatedUser)
   }
 }
