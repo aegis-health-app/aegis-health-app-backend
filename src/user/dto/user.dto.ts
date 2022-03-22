ef860bbcb66ea07f3f6b445acb28bbc224d2fe20
siradheek <siradhee_k@hotmail.com>
2022-03-22 20:28:57 +0700

    modify schemaToDto & add getPersonalInfo

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,14 +1,15 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger'
 import { Exclude, Expose, Type } from 'class-transformer'
-import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPhoneNumber } from 'class-validator'
+import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPhoneNumber, IsString } from 'class-validator'
 import { PersonalInfo } from '../user.interface'
 
 export class CreateUserDto extends PersonalInfo {
   @ApiProperty()
   @IsNotEmpty()
-  @IsPhoneNumber('TH')
+  @IsPhoneNumber()
   phone: string
   @ApiProperty()
+  @IsString()
   @IsNotEmpty()
   password: string
 }
@@ -22,33 +23,55 @@ export class UpdateRelationshipDto {
   @IsNumber()
   cid: number
 }
-export class GetUserDto extends PersonalInfo {
+export class UserDto extends PartialType(PersonalInfo) {
   @ApiProperty()
   @IsNotEmpty()
-  @IsNumber()
-  uid: number
-  @ApiProperty()
-  @IsNotEmpty()
-  @IsPhoneNumber('TH')
-  phone: string
-}
-
-export class UpdatePersonalInfoDto extends PartialType(PersonalInfo) {
-  @ApiProperty()
   @Expose()
-  @IsNotEmpty()
   @IsNumber()
   uid: number
 }
-
+export class GetUserRequest {
+  @ApiProperty()
+  @IsNotEmpty()
+  @Expose()
+  @IsNumber()
+  uid: number
+  @ApiProperty()
+  imageid: boolean
+  @ApiProperty()
+  fname: boolean
+  @ApiProperty()
+  lname: boolean
+  @ApiProperty()
+  dname: boolean
+  @ApiProperty()
+  bday: boolean
+  @ApiProperty()
+  gender: boolean
+  @ApiProperty()
+  isElderly: boolean
+  @ApiProperty()
+  healthCondition: boolean
+  @ApiProperty()
+  bloodType: boolean
+  @ApiProperty()
+  personalMedication: boolean
+  @ApiProperty()
+  allergy: boolean
+  @ApiProperty()
+  vaccine: boolean
+}
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   uid: number
 }
-
+export interface IDto<T> {
+  new (): T
+}
 export class CreateUserResponse {
   @ApiProperty()
+  @IsNumber()
   uid: number
 }
