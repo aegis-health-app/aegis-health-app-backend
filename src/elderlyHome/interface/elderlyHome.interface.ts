b1be068754e90deb098a390fad65d081c795df84
Natthorn <tum210544@gmail.com>
2022-03-23 00:54:34 +0700

    add interface

--- src/elderlyHome/interface/elderlyHome.interface.ts
+++ src/elderlyHome/interface/elderlyHome.interface.ts
@@ -3,3 +3,31 @@ export interface ElderlyHome {
     imageid: string,
     listModuleid: number[]
 }
+
+export interface Elderly{
+    uid: number,
+    dname: string,
+    imageid: string
+}
+
+export interface CaretakerHome {
+    dname: string,
+    imageid: string,
+    listElderly: Elderly[]
+}
+
+export interface ElderlyInfo {
+    imageid: string,
+    fname: string,
+    lname: string,
+    dname: string,
+    gender: string,
+    bday: Date,
+    healthCondition: string,
+    bloodType: string,
+    personalMedication: string,
+    allergy: string,
+    vaccine: string,
+    phone: string,
+    listModuleid: number[]
+}
