30fb9be4d6b75c29eb8ee700baf108f93d558190
Manapat Weeramongkolkul <hungkeng99@gmail.com>
2022-03-24 13:49:03 +0700

    add usepipe

--- src/setting/setting.controller.ts
+++ src/setting/setting.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Param, Controller, Put, Req, Res } from '@nestjs/common';
+import { Body, Param, Controller, Put, Req, Res, UsePipes, ValidationPipe } from '@nestjs/common';
 import { SettingService } from './setting.service';
 import { ChangePasswordDto, ChangePhoneNoDto } from './dto/setting.dto';
 import { ApiBearerAuth, ApiBody, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger'
@@ -13,6 +13,7 @@ export class SettingController {
   @ApiBearerAuth()
   @ApiOkResponse({ description: 'Succesful In Changing Password' })
   @ApiUnauthorizedResponse({ description: 'Failed To Change Password' })
+  @UsePipes(new ValidationPipe({ whitelist: true }))
   @Put('/changePassword')
   async changeUserPassword(@Body() passwordDto: ChangePasswordDto, @Req() req, @Res() res): Promise<string> {
     await this.settingService.changeUserPassword(passwordDto, req.user.uid)
@@ -25,6 +26,7 @@ export class SettingController {
   @ApiBody({ type: ChangePhoneNoDto })
   @ApiOkResponse({ description: 'Succesful In Changing Phone Number' })
   @ApiUnauthorizedResponse({ description: 'Failed To Change Phone Number' })
+  @UsePipes(new ValidationPipe({ whitelist: true }))
   @Put("/changePhoneNumber")
   async changePhoneNumber(@Body() phoneDto: ChangePhoneNoDto, @Req() req, @Res() res): Promise<string> {
     await this.settingService.changePhoneNumber(phoneDto, req.user.uid, req.user.token)
