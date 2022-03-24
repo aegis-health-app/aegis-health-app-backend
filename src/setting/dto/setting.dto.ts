5da769bfe9bc031172d38d3a35ba6555e7a703cf
Manapat Weeramongkolkul <hungkeng99@gmail.com>
2022-03-24 14:48:48 +0700

    remove IsNotEmpty

--- src/setting/dto/setting.dto.ts
+++ src/setting/dto/setting.dto.ts
@@ -4,20 +4,16 @@ import { ApiProperty } from "@nestjs/swagger"
 export class ChangePasswordDto {
   @ApiProperty()
   @IsString()
-  @IsNotEmpty()
   oldPassword: string
   @ApiProperty()
   @IsString() 
-  @IsNotEmpty()
   newPassword: string
 }
 export class ChangePhoneNoDto {
   @ApiProperty()
   @IsString()
-  @IsNotEmpty()
   newPhone: string
   @ApiProperty(({minLength: 6, maxLength: 6}))
   @IsString()
-  @IsNotEmpty()
   enteredPin: string
 }
