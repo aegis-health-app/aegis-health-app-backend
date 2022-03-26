9846b693377fa189e69f4da773bcfd5ca4853b44
siradheek <siradhee_k@hotmail.com>
2022-03-26 14:46:44 +0700

    update api doc for user service

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,87 +1,63 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger'
-import { Exclude, Expose, Type } from 'class-transformer'
-import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPhoneNumber, IsString } from 'class-validator'
-import { PersonalInfo } from '../user.interface'
-import { Role } from '../../common/roles'
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { Exclude, Expose } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsPhoneNumber, IsString } from 'class-validator';
+import { PersonalInfo } from '../user.interface';
 
 export class CreateUserDto extends PersonalInfo {
   @ApiProperty()
-  @IsPhoneNumber()
-  phone: string
-  @ApiProperty()
   @IsString()
-  password: string
+  password: string;
+  @ApiProperty()
+  @Expose()
+  @IsPhoneNumber()
+  phone: string;
 }
 export class UpdateRelationshipDto {
   @ApiProperty()
   @IsNumber()
-  eid: number
+  eid: number;
   @ApiProperty()
   @IsNumber()
-  cid: number
+  cid: number;
 }
 export class UserDto extends PartialType(PersonalInfo) {
   @ApiProperty()
   @IsNotEmpty()
   @Expose()
   @IsNumber()
-  uid: number
-}
-export class GetUserRequest {
+  uid: number;
   @ApiProperty()
   @Expose()
-  @IsNumber()
-  uid: number
-  @ApiProperty()
-  imageid: boolean
-  @ApiProperty()
-  fname: boolean
-  @ApiProperty()
-  lname: boolean
-  @ApiProperty()
-  dname: boolean
-  @ApiProperty()
-  bday: boolean
-  @ApiProperty()
-  gender: boolean
-  @ApiProperty()
-  isElderly: boolean
-  @ApiProperty()
-  healthCondition: boolean
-  @ApiProperty()
-  bloodType: boolean
-  @ApiProperty()
-  personalMedication: boolean
-  @ApiProperty()
-  allergy: boolean
-  @ApiProperty()
-  vaccine: boolean
+  @IsPhoneNumber()
+  phone: string;
 }
 export interface IDto<T> {
-  new (): T
+  new (): T;
 }
+
+export class UpdateUserProfileDto extends PartialType(PersonalInfo) {}
 export class CreateUserResponse {
   @ApiProperty()
   @IsNumber()
-  uid: number
+  uid: number;
 }
 
 export class LoginDto {
   @ApiProperty()
   @IsPhoneNumber()
-  phone: string
+  phone: string;
 
   @ApiProperty()
   @IsString()
-  password: string
+  password: string;
 }
 
 export class AuthResponse {
-  token: string
+  token: string;
 }
 
 export class UploadProfileResponse {
   @ApiProperty()
   @IsString()
-  url: string
+  url: string;
 }
