24eaf9924837ef08ed34f100a8825fc77f96e124
Natthorn <tum210544@gmail.com>
2022-04-04 21:48:57 +0700

    add import

--- src/healthRecord/healthRecord.module.ts
+++ src/healthRecord/healthRecord.module.ts
@@ -5,12 +5,13 @@ import { HealthColumn } from 'src/entities/healthColumn.entity';
 import { HealthData } from 'src/entities/healthData.entity';
 import { HealthRecord } from 'src/entities/healthRecord.entity';
 import { User } from 'src/entities/user.entity';
+import { GoogleModule } from 'src/google-cloud/google.module';
 import { UserModule } from 'src/user/user.module';
 import { HealthRecordController } from './healthRecord.controller';
 import { HealthRecordService } from './healthRecord.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HealthRecord, HealthColumn, HealthData, User]), UserModule],
+  imports: [TypeOrmModule.forFeature([HealthRecord, HealthColumn, HealthData, User]), UserModule, GoogleModule],
   controllers: [HealthRecordController],
   providers: [HealthRecordService],
   exports: [HealthRecordService],
