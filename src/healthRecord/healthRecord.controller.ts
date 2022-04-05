64e032ab2a6bb0dd3369dbbb114eacac880ab606
awesomeman007 <suparuek.ping@gmail.com>
2022-04-05 17:28:14 +0700

    handle error cases

--- src/healthRecord/healthRecord.controller.ts
+++ src/healthRecord/healthRecord.controller.ts
@@ -24,20 +24,23 @@ export class HealthRecordController {
 
   @UseGuards(ElderlyGuard)
   @ApiOkResponse({ type: HealthRecordTableDto })
+  @ApiNotFoundResponse({ description: 'Health Record Name Not Found' })
   @ApiParam({ name: 'healthRecordName', type: String, description: 'health record name user wants to find' })
-  @Get('/:healthRecordName')
+  @Get('/table/:healthRecordName')
   async getHealthDataByElderly(@Param('healthRecordName') healthRecordName: string, @Request() req): Promise<HealthRecordTableDto> {
-    const userId = req.user.uid;
-    return await this.healthRecordService.getHealthData(userId, healthRecordName);
+    const elderlyId = req.user.uid;
+    await this.healthRecordService.checkHealthRecordExist(Number(elderlyId), healthRecordName);
+    return await this.healthRecordService.getHealthTable(Number(elderlyId), healthRecordName);
   }
 
   @UseGuards(CaretakerGuard)
   @ApiOkResponse({ type: HealthRecordTableDto })
   @ApiBadRequestResponse({ description: "Caretaker doesn't have access to this elderly" })
   @ApiNotFoundResponse({ description: 'Elderly Does Not Exist' })
+  @ApiNotFoundResponse({ description: 'Health Record Name Not Found' })
   @ApiParam({ name: 'healthRecordName', type: String, description: 'health record name user wants to find' })
   @ApiParam({ name: 'elderlyId', type: String, description: 'id of elderly interested' })
-  @Get('/:elderlyId/:healthRecordName')
+  @Get('/table/:elderlyId/:healthRecordName')
   async getHealthDataByCaretaker(
     @Param('elderlyId') elderlyId: string,
     @Param('healthRecordName') healthRecordName: string,
@@ -45,15 +48,17 @@ export class HealthRecordController {
   ): Promise<HealthRecordTableDto> {
     const caretakerId = req.user.uid;
     await this.userService.checkRelationship(Number(elderlyId), Number(caretakerId));
-    return await this.healthRecordService.getHealthData(Number(elderlyId), healthRecordName);
+    await this.healthRecordService.checkHealthRecordExist(Number(elderlyId), healthRecordName);
+    return await this.healthRecordService.getHealthTable(Number(elderlyId), healthRecordName);
   }
 
   @UseGuards(ElderlyGuard)
   @ApiOkResponse({ type: HealthRecordAnalyticsDto })
+  @ApiNotFoundResponse({ description: 'Health Column Name Not Found' })
+  @ApiNotFoundResponse({ description: 'Health Record Name Not Found' })
   @ApiParam({ name: 'healthRecordName', type: String, description: 'health record name user wants to find' })
   @ApiParam({ name: 'columnName', description: 'column name in health record user wants to query' })
   @ApiParam({ name: 'timespan', type: String, enum: Timespan, description: 'timespan of data user wants to query' })
-  @UsePipes(new ValidationPipe({ whitelist: true }))
   @Get('analytics/:healthRecordName/:columnName/:timespan')
   async getHealthAnalyticsByElderly(
     @Param('healthRecordName') healthRecordName: string,
@@ -61,18 +66,22 @@ export class HealthRecordController {
     @Param('timespan') timespan: Timespan = Timespan.Year,
     @Request() req
   ): Promise<HealthRecordAnalyticsDto> {
-    const userId = req.user.uid;
-    return await this.healthRecordService.getHealthAnalytics(userId, healthRecordName, columnName, timespan);
+    const elderlyId = req.user.uid;
+    await this.healthRecordService.checkHealthRecordExist(Number(elderlyId), healthRecordName);
+    await this.healthRecordService.checkHealthColumnExist(Number(elderlyId), healthRecordName, columnName);
+    return await this.healthRecordService.getHealthAnalytics(Number(elderlyId), healthRecordName, columnName, timespan);
   }
 
   @UseGuards(CaretakerGuard)
   @ApiOkResponse({ type: HealthRecordAnalyticsDto })
+  @ApiNotFoundResponse({ description: 'Elderly Does Not Exist' })
+  @ApiNotFoundResponse({ description: 'Health Column Name Not Found' })
+  @ApiNotFoundResponse({ description: 'Health Record Name Not Found' })
   @ApiParam({ name: 'elderlyId', type: String, description: 'id of elderly user wants to query' })
   @ApiParam({ name: 'healthRecordName', type: String, description: 'health record name user wants to find' })
   @ApiParam({ name: 'columnName', type: String, description: 'column name in health record user wants to query' })
   @ApiParam({ name: 'timespan', type: String, enum: Timespan, description: 'timespan of data user wants to query' })
-  @UsePipes(new ValidationPipe({ whitelist: true }))
-  @Get('analytics/:elderId/:healthRecordName/:columnName/:timespan')
+  @Get('analytics/:elderlyId/:healthRecordName/:columnName/:timespan')
   async getHealthAnalyticsByCaretaker(
     @Param('elderlyId') elderlyId: string,
     @Param('healthRecordName') healthRecordName: string,
@@ -81,7 +90,10 @@ export class HealthRecordController {
     @Request() req
   ): Promise<HealthRecordAnalyticsDto> {
     const caretakerId = req.user.uid;
+    console.log(elderlyId);
     await this.userService.checkRelationship(Number(elderlyId), caretakerId);
+    await this.healthRecordService.checkHealthRecordExist(Number(elderlyId), healthRecordName);
+    await this.healthRecordService.checkHealthColumnExist(Number(elderlyId), healthRecordName, columnName);
     return await this.healthRecordService.getHealthAnalytics(Number(elderlyId), healthRecordName, columnName, timespan);
   }
 }
