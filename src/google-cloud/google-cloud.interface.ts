7aa61419380286f6473eff0c67a876cf2d2d944b
siradheek <siradhee_k@hotmail.com>
2022-04-07 12:45:35 +0700

    fix bugs

--- src/google-cloud/google-cloud.interface.ts
+++ src/google-cloud/google-cloud.interface.ts
@@ -1,6 +1,6 @@
 export enum BucketName {
-  Profile = 'aegies-user-profile',
+  Profile = 'aegis-user-profile',
   HealthRecord = 'aegis-user-health-record',
   Reminder = 'aegis-user-reminder',
-  MemoryRecall = 'aegis-user-memory-recall'
+  MemoryRecall = 'aegis-user-memory-recall',
 }
