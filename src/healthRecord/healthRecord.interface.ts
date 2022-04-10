33b39df9e6ced0ef2b84d741e40a99c07c650076
Manapat Weeramongkolkul <hungkeng99@gmail.com>
2022-04-10 14:39:06 +0700

    add isoptional

--- src/healthRecord/healthRecord.interface.ts
+++ src/healthRecord/healthRecord.interface.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsArray, IsNumber, IsObject, IsString } from "class-validator"
+import { IsArray, IsNumber, IsObject, IsOptional, IsString } from "class-validator"
 import { UploadProfileRequest } from "src/user/dto/user.dto"
+import { IsNull } from "typeorm"
 
 export class HealthRecord {
   @ApiProperty()
@@ -46,6 +47,8 @@ export class AddHealthrecord {
   @IsString()
   hrName: string
   @ApiProperty({ type: UploadImage })
+  @IsOptional()
+  @IsObject()
   picture?: UploadImage
   @ApiProperty({ type: [HealthDataField] })
   @IsArray()
